65213e703a08f604fab269524764770098eca8fa
Кожевников Антон <90034842+AntonKozhevnikov@users.noreply.github.com>
2022-02-27 17:09:08 +0400

    Add files via upload

---
+++ z1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char j=92;
+    cout<<"#include <iostream>"<< "\n"<< "\n"<<"int main() { "<< "\n"<<
+    "    std::cout << 'Hello world!"<<j<<"n' << std::endl;" << "\n" <<
+    "    return 0;" << "\n"<<
+    "}";
+    return 0;
+}
